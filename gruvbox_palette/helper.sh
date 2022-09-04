df59f10dbfc244a86ac8da0866549e63219e08c6
su55y <cesarmilano88@yahoo.com>
2022-09-04 21:04:40 +0300

    gruvbox palette launcher

---
+++ gruvbox_palette/helper.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P)"
+
+# activate markup
+echo -en "\x00markup-rows\x1ftrue\n"
+
+palette() {
+    [[ -f "$SCRIPTPATH/palette" ]] && {
+        while IFS= read -r line; do
+            color_name="${line% *}"
+            color_hex="${line#* }"
+            echo -e "<span background='$color_hex'>     </span> <span color='$color_hex'>$color_name</span>\x00info\x1f$color_hex"
+        done < "$SCRIPTPATH/palette"
+    }
+}
+
+case $ROFI_RETV in
+    # select line
+    1)
+        [[ "${ROFI_INFO}" =~ ^#[0-9a-fA-F]{6}$ ]] && \
+            echo -n "$ROFI_INFO" | xsel -ib
+    ;;
+esac
+
+# print palette on start
+[[ "${ROFI_RETV}" -eq 0 ]] && palette
---
+++ gruvbox_palette/launcher.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P)"
+
+[ -f "$SCRIPTPATH/helper.sh" ] || {
+    notify-send "rofi" "wiki helper executable not found"
+    exit 1
+}
+
+logo="$(printf "\Ue22b") "
+
+theme() { cat <<EOF
+* {
+    font: "NotoSans Nerd Font 18";
+}
+window {
+    width: 200px;
+    height: 100%;
+    location: north west;
+}
+listview {
+    scrollbar: false;
+    border: none;
+}
+inputbar {
+    spacing: 10px;
+    children: [prompt,entry];
+}
+entry {
+    placeholder: "";
+}
+EOF
+}
+
+rofi -i -show "$logo" \
+    -modi "$logo:$SCRIPTPATH/helper.sh" \
+    -no-config \
+    -theme-str "$(theme)"
