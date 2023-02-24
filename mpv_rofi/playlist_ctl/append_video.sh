e5b68b9cef23998bad8cfc44c84a0cd05923cc9d
su55y <cesarmilano88@yahoo.com>
2023-02-24 20:46:57 +0200

    playlist ctl launcher

---
+++ mpv_rofi/playlist_ctl/append_video.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+MPV_SOCKET_FILE="/tmp/mpv.sock"
+MPV_PLAYLIST_FILE="/tmp/mpv_current_playlist"
+RX_URL=".*youtube\.com\/watch\?v=([\w\d_\-]{11})|.*youtu\.be\/([\w\d_\-]{11})|.*twitch\.tv\/videos\/(\d{10})$"
+
+notify(){
+    [ -n "$1" ] && notify-send -i mpv -a mpv "$1"
+}
+
+append_play(){
+    printf '%s' "$(printf '{"command": ["loadfile", "%s", "append-play"]}\n' "$1" |\
+        nc -NU "$MPV_SOCKET_FILE" |\
+        jq .error)"
+}
+
+update_playlist(){ 
+    i=0
+    for url in $(printf '{"command": ["get_property", "playlist"]}\n' |\
+            nc -NU "$MPV_SOCKET_FILE" |\
+            jq '.data[]? | .filename'); do
+        title="$(yt-dlp -e "$(printf '%s' "$url" | sed 's/\"//g')" 2>/dev/null)" || continue
+        case $i in
+            0) echo "$i $title" > "$MPV_PLAYLIST_FILE" ;;
+            *) echo "$i $title" >> "$MPV_PLAYLIST_FILE" ;;
+        esac
+        i=$((i+1))
+    done
+
+    case $i in
+        0) notify "playlist not updated" ;;
+        *) notify "playlist successfully updated ($i)" ;;
+    esac
+}
+
+pidof mpv >/dev/null 2>&1 || {
+    [ -S "$MPV_SOCKET_FILE" ] && rm "$MPV_SOCKET_FILE"
+    setsid -f mpv --idle --no-terminal --input-ipc-server="$MPV_SOCKET_FILE"
+
+    for _ in $(seq 10); do
+        [ -S "$MPV_SOCKET_FILE" ] && pidof mpv >/dev/null 2>&1 && break
+        sleep 0.1
+    done
+
+    [ -S "$MPV_SOCKET_FILE" ] || {
+        notify "can't find mpv socket file"
+        killall mpv
+        exit 1
+    }
+}
+
+url="$(xclip -o -selection clipboard)"
+[ "$(printf '%s' "$url" |\
+    grep -oP "$RX_URL")" = "$url" ] && {
+        case $(append_play "$url") in
+            *success*)
+                notify "video added to playlist: $url"
+                update_playlist
+            ;;
+            *) notify "can't add video: $url" ;;
+        esac
+}
---
+++ mpv_rofi/playlist_ctl/helper.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+MPV_SOCKET_FILE="/tmp/mpv.sock"
+MPV_PLAYLIST_FILE="/tmp/mpv_current_playlist"
+
+
+play_index(){
+    [ -S "$MPV_SOCKET_FILE" ] || printf 'sock' && return
+    printf '%s' "$(printf '{"command": ["playlist-play-index", "%s"]}\n' "$1" |\
+        nc -NU "$MPV_SOCKET_FILE" |\
+        jq .error)"
+}
+
+print_playlist(){
+    [ -f "$MPV_PLAYLIST_FILE" ] || {
+        printf '%s not found\000nonselectable\037true\n' "$MPV_PLAYLIST_FILE"
+        return
+    }
+    while read -r entry || [ "$entry" ]; do
+        id="${entry%% *}"
+        title="${entry#* }"
+        printf '%s\000info\037%s\n' "$title" "$id"
+    done < "$MPV_PLAYLIST_FILE"
+}
+
+case $ROFI_RETV in
+    0) print_playlist ;;
+    1)
+        case $(play_index "$ROFI_INFO") in
+            *success*) exit 0 ;;
+            sock) echo "$MPV_SOCKET_FILE not found" ;;
+            *)
+                pidof mpv >/dev/null 2>&1 || echo "mpv process not found"
+                echo "some error occurred, check logs"
+            ;;
+        esac
+    ;;
+esac
