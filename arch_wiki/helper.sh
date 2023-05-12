7e6361299d9cac1d04ff9c19174a32f077045e5b
su55y <cesarmilano88@yahoo.com>
2023-05-12 20:09:11 +0300

    unified launcher

---
+++ arch_wiki/helper.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+WIKIDIR="/usr/share/doc/arch-wiki/html/en/"
+BROWSER="${BROWSER:-qutebrowser}"
+
+printf "\000use-hot-keys\037true\n"
+
+print_list() {
+    find "$WIKIDIR" -iname '*.html' -printf '%f %p\n' |\
+        awk '{
+            gsub("_"," ",$1);
+            sub(/\.html$/,"",$1);
+            printf "%s\000info\037%s\n", $1, $NF}' |\
+        sort -g
+}
+
+case $ROFI_RETV in
+    0) print_list ;;
+    1)
+        [ -f "$ROFI_INFO" ] || {
+            notify-send -a "Arch Wiki Launcher" "can't find '$ROFI_INFO'"
+            exit 1
+        }
+        setsid -f "$BROWSER" "$ROFI_INFO" >/dev/null 2>&1
+    ;;
+    10) printf '%s' "$ROFI_INFO" | xsel -i -b ;;
+esac
---
+++ arch_wiki/launcher.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P)"
+
+[ -f "$SCRIPTPATH/helper.sh" ] || {
+    notify-send -i "rofi" -a "arch wiki" "helper script not found"
+    exit 1
+}
+
+theme() { cat <<EOF
+configuration {
+  font: "NotoSans Nerd Font 18";
+}
+inputbar {
+  children: ["textbox-prompt-colon","entry","num-filtered-rows","textbox-num-sep","num-rows","case-indicator"];
+}
+textbox-prompt-colon {
+  str: "";
+}
+EOF
+}
+
+rofi -i -show "wiki" \
+    -modi "wiki:$SCRIPTPATH/helper.sh" \
+    -no-config \
+    -no-custom \
+    -sort true \
+    -kb-custom-1 "Ctrl+c" \
+    -theme-str "$(theme)"
