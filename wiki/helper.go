4cad04c81d5bf1226dc8a45ae2b767d7ed1e2f80
su55y <cesarmilano88@yahoo.com>
2023-08-23 19:55:45 +0300

    wiki helper.go refactoring

--- wiki/helper.go
+++ wiki/helper.go
@@ -2,77 +2,97 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
+	"sync"
+	"time"
 )
 
 const (
-	apiUrl  = "https://%s.wikipedia.org/w/api.php?action=opensearch&namespace=0&format=json&formatversion=2&limit=10&search=%s"
-	errRead = "<span color='#f00' weight='bold'>[ERROR]</span> can't read response 😧\000nonselectable\037true"
-	errReq  = "<span color='#f00' weight='bold'>[ERROR]</span> bad request 😠\000nonselectable\037true"
+	apiUrl         = "https://%s.wikipedia.org/w/api.php?action=opensearch&namespace=0&format=json&formatversion=2&limit=%d&search=%s"
+	entryFmt       = "<span weight='bold'>%d</span>) %s\000icon\037%s_wiki\037info\037%s\n"
+	errMessageRofi = "\000message\037error: %s\n"
+	searchFmt      = "<span weight='bold'>-</span>) search\000icon\037%s_wiki\037info\037https://%s.wikipedia.org/wiki/Special:Search\n"
 )
 
-var langs = []map[string]string{
-	{
-		"lang":   "uk",
-		"empty":  "<span color='gray'>нічого не знайдено</span> 😴\000icon\037ua_square\037nonselectable\037true",
-		"format": "<span weight='bold'>%d</span>) %s\000icon\037ua_square\037info\037%s\n",
-	},
-	{
-		"lang":   "en",
-		"empty":  "<span color='gray'>nothing found</span> 😴\000icon\037uk_square\037nonselectable\037true",
-		"format": "<span weight='bold'>%d</span>) %s\000icon\037uk_square\037info\037%s\n",
-	},
+var (
+	limit int
+	langs = [2]string{"uk", "en"}
+	query string
+)
+
+type Article struct {
+	Title, Url string
 }
 
-type SearchResult struct {
-	Search           string
-	Titles, Links, S []string
+func fetchArticles(c *http.Client, u string) ([]Article, error) {
+	resp, err := c.Get(u)
+	if err != nil {
+		return nil, fmt.Errorf("can't fetch articles: %v", err)
+	}
+	defer func() { _ = resp.Body.Close() }()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("can't read response: %v", err)
+	}
+
+	var titles, urls []string
+	if err := json.Unmarshal(body, &[]interface{}{nil, &titles, nil, &urls}); err != nil {
+		return nil, fmt.Errorf("can't parse articles: %v", err)
+	}
+
+	if len(titles) != len(urls) {
+		return nil, fmt.Errorf("count of titles (%d) != urls (%d)", len(titles), len(urls))
+	}
+
+	articles := make([]Article, len(titles))
+	for i := 0; i < len(titles); i++ {
+		articles[i] = Article{Title: titles[i], Url: urls[i]}
+	}
+
+	return articles, nil
 }
 
-func parseUrl(lang string, query string) string {
-	return fmt.Sprintf(apiUrl, lang, query)
-}
-
-func formatOutput(result SearchResult, l int) {
-	if len(result.Links) == 0 {
-		fmt.Println(langs[l]["empty"])
+func fetchAndPrint(client *http.Client, wg *sync.WaitGroup, lang string) {
+	defer wg.Done()
+	articles, err := fetchArticles(client, fmt.Sprintf(apiUrl, lang, limit, url.QueryEscape(query)))
+	if err != nil {
+		fmt.Printf(errMessageRofi, err.Error()[:79])
+		fmt.Printf(searchFmt, lang, lang)
 		return
 	}
-
-	for i, link := range result.Links {
-		fmt.Printf(langs[l]["format"], i+1, result.Titles[i], link)
+	if len(articles) == 0 {
+		fmt.Printf(searchFmt, lang, lang)
+		return
+	}
+	for i, a := range articles {
+		fmt.Printf(entryFmt, i+1, a.Title, lang, a.Url)
 	}
 }
 
-func find(subj string) {
-	for i, lang := range langs {
-		response, err := http.Get(parseUrl(lang["lang"], url.QueryEscape(subj)))
-		if err != nil {
-			fmt.Println(errReq)
-			continue
-		}
-		defer response.Body.Close()
-		var result SearchResult
-		if err := json.NewDecoder(response.Body).Decode(&[]interface{}{
-			&result.Search,
-			&result.Titles,
-			&result.S,
-			&result.Links,
-		}); err != nil {
-			fmt.Println(errRead)
-			continue
-		}
-		formatOutput(result, i)
-	}
+func die(err error) {
+	fmt.Println(err)
+	os.Exit(1)
 }
 
 func main() {
-	if len(os.Args[1]) == 0 {
-		os.Exit(1)
+	flag.StringVar(&query, "q", "", "search query (required)")
+	flag.IntVar(&limit, "l", 10, "set results count limit for each request")
+	flag.Parse()
+	if len(query) == 0 {
+		die(fmt.Errorf("query is empty"))
 	}
 
-	find(os.Args[1])
+	client := &http.Client{Timeout: time.Second * 5}
+	wg := sync.WaitGroup{}
+	for _, lang := range langs {
+		wg.Add(1)
+		go fetchAndPrint(client, &wg, lang)
+	}
+	wg.Wait()
 }
