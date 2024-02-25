c5a0a9fd85baf977a71e0d32034390527b8f79ba
su55y <cesarmilano88@yahoo.com>
2024-02-26 00:04:21 +0200

    lxqtnotifdlog: html escape

--- lxqtnotiflog/helper.py
+++ lxqtnotiflog/helper.py
@@ -2,9 +2,10 @@ from dataclasses import dataclass, fields
 import datetime as dt
 import os.path
 import re
-import urllib.parse
+import html
 
 UNATTENDED_LIST = os.path.expanduser("~/.cache/lxqt-notificationd/unattended.list")
+FMT = "<b>{app}</b> <i>{created}</i>\r{text}\000icon\037{icon}\037info\037{info}"
 
 
 @dataclass(slots=True)
@@ -38,24 +39,18 @@ def build_info(e: Entry) -> str:
     return f"-i {e.icon!r} -a {e.application!r} {e.summary!r} {e.body!r}"
 
 
-def log(msg: str):
-    with open("/tmp/tmp_py.log", "a") as f:
-        f.write(msg)
-
-
 if __name__ == "__main__":
     if not os.path.exists(UNATTENDED_LIST):
         print("\000message\037error: %r not found" % UNATTENDED_LIST, end="\012")
         exit(1)
     rx_lines = re.compile(r"^(\[|App|Summ|Icon|Body).+")
     lines = []
-    with open(UNATTENDED_LIST) as f:
+    with open(UNATTENDED_LIST, "r", encoding="utf-8") as f:
         lines = list(filter(lambda l: rx_lines.match(l), f.readlines()))
 
     if len(lines) % 5 != 0:
         print(
-            "\000message\037error: unexpected dunstlog format (%d % 5 != 0)"
-            % len(lines),
+            f"\000message\037error: unexpected file format ({len(lines)} % 5 != 0)",
             end="\012",
         )
         exit(1)
@@ -64,16 +59,20 @@ if __name__ == "__main__":
         Entry(
             created=lines[i].strip(),
             application=lines[i + 1].strip(),
-            body=lines[i + 2].strip(),
+            body=html.escape(lines[i + 2].strip()),
             icon=lines[i + 3].strip(),
-            summary=lines[i + 4].strip(),
+            summary=html.escape(lines[i + 4].strip()),
         )
         for i in range(0, len(lines), 5)
     ]
     for e in entries[::-1]:
-        log(f"{e}\n")
         print(
-            "<b>%s</b> <i>%s</i>\r%s\000icon\037%s\037info\037%s"
-            % (e.application, e.created, e.body or e.summary, e.icon, build_info(e)),
+            FMT.format(
+                app=e.application,
+                created=e.created,
+                text=e.body or e.summary,
+                icon=e.icon,
+                info=build_info(e),
+            ),
             end="\012",
         )
