d5a4e02d7ac27d974ebb643920adf08e33e47ffe
su55y <cesarmilano88@yahoo.com>
2024-06-09 11:48:32 +0300

    arch_wiki: fmt

--- arch_wiki/helper.sh
+++ arch_wiki/helper.sh
@@ -11,39 +11,39 @@ err_msg() {
 }
 
 print_list() {
-    find "$WIKIDIR" -iname '*.html' -printf '%f %p\n' |\
-        awk '{
+  find "$WIKIDIR" -iname '*.html' -printf '%f %p\n' |
+    awk '{
             gsub("_"," ",$1);
             sub(/\.html$/,"",$1);
-            printf "%s\000info\037%s\n", $1, $NF}' |\
-        sort -g
+            printf "%s\000info\037%s\n", $1, $NF}' |
+    sort -g
 }
 
 case $ROFI_RETV in
-    0) print_list ;;
-    1)
-        [ -f "$ROFI_INFO" ] || {
-            notify-send -i "rofi" -a "arch wiki" "can't find '$ROFI_INFO'"
-            exit 1
-        }
-        setsid -f "$BROWSER" "$ROFI_INFO" >/dev/null 2>&1
-        print_list
-    ;;
-    10) printf '%s' "$ROFI_INFO" | xsel -i -b ;;
-    11)
-        [ -d "$CACHE_DIR" ] || mkdir -p "$CACHE_DIR" >/dev/null 2>&1
-        clean_article="$(basename "$ROFI_INFO")"
-        clean_article="${clean_article%.*}"
-        pdf_path="$CACHE_DIR/${clean_article}.pdf"
-        if [ -f "$pdf_path" ]; then
-          setsid -f zathura "$pdf_path" >/dev/null 2>&1 || err_msg "can't open in zathura"
-        else
-          [ -f "$ROFI_INFO" ] || err_msg "ERROR: '$ROFI_INFO' not found"
-          clean_article_path="/tmp/${clean_article}.html"
-          rdrview -T title,body -H -u "$1" <"$ROFI_INFO" >"$clean_article_path" 2>/dev/null || err_msg "readability conversion error"
-          pandoc "$clean_article_path" -t ms -o "$pdf_path" 2>/dev/null || err_msg "pandoc error"
-          setsid -f zathura "$pdf_path" >/dev/null 2>&1 || err_msg "can't open in zathura"
-        fi
-        print_list
-      ;;
+0) print_list ;;
+1)
+  [ -f "$ROFI_INFO" ] || {
+    notify-send -i "rofi" -a "arch wiki" "can't find '$ROFI_INFO'"
+    exit 1
+  }
+  setsid -f "$BROWSER" "$ROFI_INFO" >/dev/null 2>&1
+  print_list
+  ;;
+10) printf '%s' "$ROFI_INFO" | xsel -i -b ;;
+11)
+  [ -d "$CACHE_DIR" ] || mkdir -p "$CACHE_DIR" >/dev/null 2>&1
+  clean_article="$(basename "$ROFI_INFO")"
+  clean_article="${clean_article%.*}"
+  pdf_path="$CACHE_DIR/${clean_article}.pdf"
+  if [ -f "$pdf_path" ]; then
+    setsid -f zathura "$pdf_path" >/dev/null 2>&1 || err_msg "can't open in zathura"
+  else
+    [ -f "$ROFI_INFO" ] || err_msg "ERROR: '$ROFI_INFO' not found"
+    clean_article_path="/tmp/${clean_article}.html"
+    rdrview -T title,body -H -u "$1" <"$ROFI_INFO" >"$clean_article_path" 2>/dev/null || err_msg "readability conversion error"
+    pandoc "$clean_article_path" -t ms -o "$pdf_path" 2>/dev/null || err_msg "pandoc error"
+    setsid -f zathura "$pdf_path" >/dev/null 2>&1 || err_msg "can't open in zathura"
+  fi
+  print_list
+  ;;
 esac
--- arch_wiki/launcher.sh
+++ arch_wiki/launcher.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1 ; pwd -P)"
+SCRIPTPATH="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+  pwd -P
+)"
 
 [ -f "$SCRIPTPATH/helper.sh" ] || {
-    notify-send -i "rofi" -a "arch wiki" "helper script not found"
-    exit 1
+  notify-send -i "rofi" -a "arch wiki" "helper script not found"
+  exit 1
 }
 
-theme() { cat <<EOF
+theme() {
+  cat <<EOF
 configuration {
   font: "NotoSans Nerd Font 18";
 }
@@ -21,12 +25,12 @@ EOF
 }
 
 rofi -i -show "wiki" \
-    -modi "wiki:$SCRIPTPATH/helper.sh" \
-    -no-config \
-    -no-custom \
-    -sort true \
-    -kb-custom-1 "Ctrl+c" \
-    -kb-row-select "" \
-    -kb-custom-2 "Ctrl+space" \
-    -theme-str "$(theme)" \
-    -normal-window
+  -modi "wiki:$SCRIPTPATH/helper.sh" \
+  -no-config \
+  -no-custom \
+  -sort true \
+  -kb-custom-1 "Ctrl+c" \
+  -kb-row-select "" \
+  -kb-custom-2 "Ctrl+space" \
+  -theme-str "$(theme)" \
+  -normal-window
