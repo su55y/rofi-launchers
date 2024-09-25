ff98ffd0ff961a221dc673bc93e62298db868bb5
su55y <cesarmilano88@yahoo.com>
2024-09-25 21:57:59 +0300

    translate launcher: allow rofi prompt and trans commands via env

--- translate/launcher.sh
+++ translate/launcher.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+[ -n "$ROFI_PROMPT_CMD" ] || ROFI_PROMPT_CMD="rofi -dmenu -p trans -theme-str 'listview {lines: 0;}'"
+[ -n "$TRANS_CMD" ] || TRANS_CMD="trans -no-ansi en:uk '%s'"
+
 while true; do
-	inp="$(rofi -dmenu -p trans -theme-str 'listview {lines: 0;}')"
+	inp="$(eval "$ROFI_PROMPT_CMD" 2>/dev/null)"
 	[ -z "$inp" ] && exit 0
-	rofi -e "$(trans -no-ansi :uk "$inp")"
+
+	# shellcheck disable=SC2059
+	TRANS_CMD_="$(printf "$TRANS_CMD" "$inp")"
+	rofi -e "$(eval "$TRANS_CMD_")"
 done
