1c4a6996db9c5fd1401f4daaf25d6532febad325
su55y <cesarmilano88@yahoo.com>
2024-09-26 20:32:17 +0300

    translate launcher: shellcheck fmt

--- translate/launcher.sh
+++ translate/launcher.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
+# shellcheck disable=SC2059
+# SC2059: Don't use variables in the printf format string. Use printf "..%s.." "$foo".
 
 [ -n "$ROFI_PROMPT_CMD" ] || ROFI_PROMPT_CMD="rofi -dmenu -p trans -theme-str 'listview {lines: 0;}'"
 [ -n "$TRANS_CMD" ] || TRANS_CMD="trans -no-ansi en:uk '%s'"
 [ -n "$ROFI_RESULT_CMD" ] || ROFI_RESULT_CMD="rofi -e '%s'"
 
 translate_() {
-	# shellcheck disable=SC2059
 	TRANS_CMD_="$(printf "$TRANS_CMD" "$1")"
 	result="$(eval "$TRANS_CMD_")"
 
-	# shellcheck disable=SC2059
 	ROFI_RESULT_CMD_="$(printf "$ROFI_RESULT_CMD" "$result")"
 	eval "$ROFI_RESULT_CMD_"
 }
