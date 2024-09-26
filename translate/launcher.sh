26b97a15cf5c8873c890d1ef404ef0f087659325
su55y <cesarmilano88@yahoo.com>
2024-09-26 20:25:42 +0300

    translate launcher: translate_ method

--- translate/launcher.sh
+++ translate/launcher.sh
@@ -3,11 +3,15 @@
 [ -n "$ROFI_PROMPT_CMD" ] || ROFI_PROMPT_CMD="rofi -dmenu -p trans -theme-str 'listview {lines: 0;}'"
 [ -n "$TRANS_CMD" ] || TRANS_CMD="trans -no-ansi en:uk '%s'"
 
+translate_() {
+	# shellcheck disable=SC2059
+	TRANS_CMD_="$(printf "$TRANS_CMD" "$1")"
+	rofi -e "$(eval "$TRANS_CMD_")"
+}
+
 while true; do
 	inp="$(eval "$ROFI_PROMPT_CMD" 2>/dev/null)"
 	[ -z "$inp" ] && exit 0
 
-	# shellcheck disable=SC2059
-	TRANS_CMD_="$(printf "$TRANS_CMD" "$inp")"
-	rofi -e "$(eval "$TRANS_CMD_")"
+	translate_ "$inp"
 done
