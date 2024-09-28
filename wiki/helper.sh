a267ceef0622772739b63e5923dc1fc70f3970eb
su55y <cesarmilano88@yahoo.com>
2024-09-28 20:11:53 +0300

    wiki: mark cached lines

--- wiki/helper.sh
+++ wiki/helper.sh
@@ -27,6 +27,7 @@ banner() {
 
 print_from_cache() {
 	[ -f "$1" ] || err_msg "no recent results found in cache"
+	printf '\000message\037[Cache]\n'
 	printf '\000data\037%s\n' "$1"
 	awk '{gsub(/\\000/, "\0"); gsub(/\\037/, "\037"); print}' "$1"
 }
