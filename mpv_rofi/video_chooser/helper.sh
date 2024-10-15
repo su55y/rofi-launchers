8cca88dabd7f692932aba0c508c5c8e02dacf940
su55y <cesarmilano88@yahoo.com>
2024-10-15 23:27:23 +0300

    mpv_rofi/video_chooser: fmt

--- mpv_rofi/video_chooser/helper.sh
+++ mpv_rofi/video_chooser/helper.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 SCRIPTPATH="$(
-	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
-	pwd -P
+    cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+    pwd -P
 )"
 . "${SCRIPTPATH}/../mpv_rofi_utils"
 
@@ -15,21 +15,21 @@ printf "\000use-hot-keys\037true\012"
 printf "\000markup-rows\037true\012"
 
 print_from_cache() {
-	printf '\000message\037[Cache]\n'
-	awk '{gsub(/\\000/, "\0"); gsub(/\\037/, "\037"); gsub(/\\012/, "\012"); print}' "$TEMPFILE"
+    printf '\000message\037[Cache]\n'
+    awk '{gsub(/\\000/, "\0"); gsub(/\\037/, "\037"); gsub(/\\012/, "\012"); print}' "$TEMPFILE"
 }
 
 printer() {
-	if [ -f "$TEMPFILE" ]; then
-		print_from_cache
-	else
-		"${SCRIPTPATH}/printer" "$VIDSDIR" | tee "$TEMPFILE"
-	fi
+    if [ -f "$TEMPFILE" ]; then
+        print_from_cache
+    else
+        "${SCRIPTPATH}/printer" "$VIDSDIR" | tee "$TEMPFILE"
+    fi
 }
 
 restart_with_filter() {
-	parent="$(basename "$(dirname "$ROFI_INFO")")"
-	setsid -f "${SCRIPTPATH}/launcher.sh" "${parent%% *}" >/dev/null 2>&1
+    parent="$(basename "$(dirname "$ROFI_INFO")")"
+    setsid -f "${SCRIPTPATH}/launcher.sh" "${parent%% *}" >/dev/null 2>&1
 }
 
 case $ROFI_RETV in
@@ -39,21 +39,21 @@ case $ROFI_RETV in
 1) [ -f "$ROFI_INFO" ] && _play "$ROFI_INFO" ;;
 # kb-custom-1 (Ctrl+a) - append to playlist and restart with filter
 10)
-	if [ -f "$ROFI_INFO" ]; then
-		restart_with_filter
-		_append "$ROFI_INFO"
-	fi
-	;;
+    if [ -f "$ROFI_INFO" ]; then
+        restart_with_filter
+        _append "$ROFI_INFO"
+    fi
+    ;;
 # kb-custom-2 (Ctrl+space) - play and restart with filter
 11)
-	if [ -f "$ROFI_INFO" ]; then
-		_play "$ROFI_INFO"
-		restart_with_filter
-	fi
-	;;
+    if [ -f "$ROFI_INFO" ]; then
+        _play "$ROFI_INFO"
+        restart_with_filter
+    fi
+    ;;
 # kb-custom-3 (Ctrl+r) - remove cache
 12)
-	rm -f "$TEMPFILE" >/dev/null 2>&1
-	printer
-	;;
+    rm -f "$TEMPFILE" >/dev/null 2>&1
+    printer
+    ;;
 esac
--- mpv_rofi/video_chooser/launcher.sh
+++ mpv_rofi/video_chooser/launcher.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 SCRIPTPATH="$(
-	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
-	pwd -P
+    cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit 1
+    pwd -P
 )"
 
 [ -f "$SCRIPTPATH/helper.sh" ] || {
-	notify-send -i "rofi" -a "video chooser" "helper script not found"
-	exit 1
+    notify-send -i "rofi" -a "video chooser" "helper script not found"
+    exit 1
 }
 
 theme() {
-	cat <<EOF
+    cat <<EOF
 window {
   font: "BlexMono Nerd Font 20";
 }
@@ -35,7 +35,7 @@ ROFI_CMD="rofi -i -no-config -show vchooser -modi 'vchooser:$SCRIPTPATH/helper.s
   -kb-custom-1 'Ctrl+a' -kb-custom-2 'Ctrl+space' -kb-custom-3 'Ctrl+r'"
 
 if [ -n "$*" ]; then
-	ROFI_CMD="$ROFI_CMD -filter '$*'"
+    ROFI_CMD="$ROFI_CMD -filter '$*'"
 fi
 
 _=$(eval "$ROFI_CMD" >/dev/null 2>&1)
