91d79be9652cd13724dea1deb4b1b4e21a21ed17
su55y <su55y@protonmail.com>
2025-08-02 15:56:55 +0300

    mpv/append_script: accept ids

--- mpv/playlist_ctl/examples/rofi/append_video.sh
+++ mpv/playlist_ctl/examples/rofi/append_video.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-MPV_START_LOCK="/tmp/mpv_start.lock"
-MPV_SOCKET_FILE="/tmp/mpv.sock"
-RX_URL=".*youtube\.com\/watch\?v=([\w\d_\-]{11})|.*youtu\.be\/([\w\d_\-]{11})"
+MPV_START_LOCK=/tmp/mpv_start.lock
+MPV_SOCKET_FILE=/tmp/mpv.sock
+RX_URL='.*youtube\.com\/watch\?v=([-_0-9a-zA-Z]{11})|.*youtu\.be\/([-_0-9a-zA-Z]{11})'
 
 notify() { [ -n "$1" ] && notify-send -i mpv -a append-script "$1"; }
 
 [ -f "$MPV_START_LOCK" ] && {
-    notify "MPV process already running..."
+    notify 'MPV process already running...'
     exit 1
 }
 
@@ -17,6 +17,7 @@ trap cleanup EXIT
 input="$1"
 [ -z "$input" ] && input="$(xclip -o -selection clipboard)"
 if [ ! -f "$input" ]; then
+    [ ${#input} -eq 11 ] && input=https://youtu.be/$input
     echo "$input" | grep -sqP "$RX_URL" || {
         notify "invalid url '$input'"
         exit 1
@@ -48,4 +49,4 @@ echo '{"command":["get_property","pause"]}' | nc -NU "$MPV_SOCKET_FILE" >/dev/nu
     exit 1
 }
 
-notify "$(playlist-ctl -a "$URL")"
+notify "$(playlist-ctl -a "$URL" 2>&1)"
