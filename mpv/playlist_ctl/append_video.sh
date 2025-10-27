def3f01744098bf0bbac6d41aa95263697ccc54a
su55y <su55y@protonmail.com>
2025-10-27 17:43:32 +0200

    mpv/append_script: allow any input

--- mpv/playlist_ctl/append_video.sh
+++ mpv/playlist_ctl/append_video.sh
@@ -2,7 +2,6 @@
 
 MPV_START_LOCK=/tmp/mpv_start.lock
 MPV_SOCKET_FILE=/tmp/mpv.sock
-RX_URL='.*youtube\.com\/watch\?v=([-_0-9a-zA-Z]{11})|.*youtu\.be\/([-_0-9a-zA-Z]{11})|.*youtube\.com\/playlist\?list=[-_0-9a-zA-Z]{18,34}|.*youtube\.com\/shorts\/[-_0-9a-zA-Z]{11}|.*twitch\.tv\/(?:videos\/\d{10}|[^?\/]+)'
 
 notify() { [ -n "$1" ] && notify-send -i mpv -a append-script "$1"; }
 
@@ -13,19 +12,6 @@ notify() { [ -n "$1" ] && notify-send -i mpv -a append-script "$1"; }
 
 cleanup() { [ -f "$MPV_START_LOCK" ] && rm "$MPV_START_LOCK"; }
 trap cleanup EXIT
-
-input="$1"
-[ -z "$input" ] && input="$(xclip -o -selection clipboard)"
-if [ -f "$input" ] || [ -d "$input" ]; then
-    URL="$input"
-else
-    [ ${#input} -eq 11 ] && input=https://youtu.be/$input
-    echo "$input" | grep -sqP "$RX_URL" || {
-        notify "invalid url '$input'"
-        exit 1
-    }
-    URL="$(echo "$input" | grep -oP "$RX_URL")"
-fi
 
 echo '{"command":["get_property","pause"]}' | nc -NU "$MPV_SOCKET_FILE" >/dev/null 2>&1 || {
     touch "$MPV_START_LOCK"
@@ -49,4 +35,4 @@ echo '{"command":["get_property","pause"]}' | nc -NU "$MPV_SOCKET_FILE" >/dev/nu
     exit 1
 }
 
-notify "$(playlist-ctl -a "$URL" 2>&1)"
+notify "$(playlist-ctl -a "$1" 2>&1)"
