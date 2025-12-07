2f3a031f39bfa4a1fc840cfa0264ad1501373368
su55y <su55y@protonmail.com>
2025-12-07 18:15:06 +0200

    mpv/video_chooser: printer.go cleanup

--- mpv/video_chooser/printer.go
+++ mpv/video_chooser/printer.go
@@ -9,43 +9,43 @@ import (
 	"strings"
 )
 
-func exitWithError(f string, args ...interface{}) {
-	fmt.Printf(
-		"\000message\037err: %s\n \000nonselectable\037true\n",
-		fmt.Sprintf(f, args...),
-	)
+const (
+	errMsgFmt = "\000message\037err: %s\n \000nonselectable\037true\n"
+	lineFmt   = "<b>%s</b>\r%s <i>%s</i>\000info\037%s\037meta\037%s\n"
+)
+
+func exitWithError(f string, args ...any) {
+	fmt.Printf(errMsgFmt, fmt.Sprintf(f, args...))
 	os.Exit(1)
 }
 
 func main() {
-	var rootDir string
-	if args := os.Args[1:]; len(args) != 1 {
-		exitWithError("invalid args count %d", len(args))
-	} else {
-		rootDir = args[0]
+	if len(os.Args[1:]) != 1 {
+		fmt.Printf("usage: %s ROOT_DIR\n", filepath.Base(os.Args[0]))
+		os.Exit(1)
 	}
+
+	rootDir := os.Args[1]
 	info, err := os.Stat(rootDir)
 	if err != nil {
-		exitWithError("%s", err.Error())
+		exitWithError(err.Error())
 	}
 	if !info.IsDir() {
 		exitWithError("%#v is not a directory", rootDir)
 	}
 
 	if err := filepath.WalkDir(rootDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
+		if d.IsDir() {
+			return nil
 		}
-		if !d.IsDir() {
-			baseName := filepath.Base(path)
-			title := html.EscapeString(strings.TrimSuffix(baseName, filepath.Ext(baseName)))
-			parentDir := filepath.Base(filepath.Dir(path))
-			relativePath := strings.TrimSuffix(strings.TrimPrefix(filepath.Dir(path), rootDir), parentDir)
-			meta := strings.Join(strings.Split(parentDir, "/"), ",")
-			fmt.Printf("<b>%s</b>\r%s <i>%s</i>\000info\037%s\037meta\037%s\n", title, parentDir, relativePath, path, meta)
-		}
+		baseName := filepath.Base(path)
+		title := html.EscapeString(strings.TrimSuffix(baseName, filepath.Ext(baseName)))
+		parentDir := filepath.Base(filepath.Dir(path))
+		relativePath := strings.TrimSuffix(strings.TrimPrefix(filepath.Dir(path), rootDir), parentDir)
+		meta := strings.Join(strings.Split(parentDir, "/"), ",")
+		fmt.Printf(lineFmt, title, parentDir, relativePath, path, meta)
 		return nil
 	}); err != nil {
-		exitWithError("%s", err.Error())
+		exitWithError(err.Error())
 	}
 }
