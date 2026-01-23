80885b704f3d8c030159bc570bbe9ade8c0c31af
su55y <su55y@protonmail.com>
2026-01-23 18:49:56 +0200

    zathura/man_viewer: cleanup

--- zathura/man_viewer/helper.sh
+++ zathura/man_viewer/helper.sh
@@ -5,26 +5,30 @@ TERM_PAGER='nvim +Man! -u NORC +color\ retrobox'
 [ -n "$ROFI_MAN_VIEWER_CACHE" ] ||
     ROFI_MAN_VIEWER_CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/rofi_man_viewer"
 if [ ! -d "$ROFI_MAN_VIEWER_CACHE" ]; then
-    mkdir -p "$ROFI_MAN_VIEWER_CACHE" || exit 1
+    mkdir -p "$ROFI_MAN_VIEWER_CACHE" 2>&1 || exit 1
 fi
 
 printf '\000use-hot-keys\037true\n'
 
-case $ROFI_RETV in
-# print manpages on start
-0) man -k . | awk '! /[_:]/{print $1}' | sort ;;
-# select line
-1)
-    [ -n "$1" ] || exit 0
+open_as_pdf() {
+    [ -n "$1" ] || exit 1
     filepath="$ROFI_MAN_VIEWER_CACHE/$1.pdf"
     if [ ! -f "$filepath" ]; then
         man -Tpdf "$1" >"$filepath" 2>/dev/null || exit 1
     fi
     setsid -f zathura "$filepath" >/dev/null 2>&1
-    ;;
-# ctrl+space - open selected in terminal
-10)
+}
+
+open_in_terminal() {
     [ -n "$1" ] || exit 0
     setsid -f "$TERMINAL" -e sh -c "man --pager='$TERM_PAGER' $1" >/dev/null 2>&1
-    ;;
+}
+
+case $ROFI_RETV in
+# print manpages on start
+0) man -k . | awk '$1 !~ /[_:]/ {print $1}' | sort ;;
+# select line
+1) open_as_pdf "$1" ;;
+# ctrl+space - open selected in terminal
+10) open_in_terminal "$1" ;;
 esac
